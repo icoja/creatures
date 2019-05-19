ec3923aba31343dd648d54138db43f84d30c1f79
lsn-mgl <alessandro.meglio2@gmail.com>
2019-05-19 04:37:30 +0200

    Fully translated mike to C

--- mike.c
+++ mike.c
@@ -1,111 +1,222 @@
 #include "mike.h"
 #include <math.h>
-#include <chipmunk/chipmunk_private.h>
+#include <stdlib.h>
 #include <stdio.h>
 
+#define MIKE_COLLISION_GROUP 16
 
-static const double mike_upper_leg_length = 40;
-static const double mike_upper_leg_thickness = 3;
-static const double mike_lower_leg_length = 40;
-static const double mike_lower_leg_thickness = 3;
-static const double mike_head_radius = 10;
-static const double mike_upper_leg_density = 0.1;
-static const double mike_lower_leg_density = 0.1;
-static const double mike_head_density = 0.1;
-static const double mike_friction = 0.8;
-//static const double mike_bounciness = 0.1;
-static const double mike_upper_joint_stiffness = 18000;
-static const double mike_lower_joint_stiffness = 18000;
-static const double mike_upper_joint_damping = 60000;
-static const double mike_lower_joint_damping = 60000;
+//mike specific shape is hard coded
+static const float upper_leg_length = 40;
+static const float upper_leg_thickness = 3;
+static const float lower_leg_length = 40;
+static const float lower_leg_thickness = 3;
+static const float head_radius = 10;
+static const float upper_leg_density = 0.1;
+static const float lower_leg_density = 0.1;
+static const float head_density = 0.1;
+static const float friction = 0.8;
+static const float bounciness = 0.1;
+static const float upper_joint_stiffness = 18000;
+static const float lower_joint_stiffness = 18000;
+static const float upper_joint_damping = 60000;
+static const float lower_joint_damping = 60000;
 
 
-void mike_init(mike_s *mike, cpSpace* space, float x, float y)
+
+void mike_init(mike_s *m)
 {
-	//TODO deleting mike is a mess
-	//go through each instance of cp*New()
-	//and make sure that everything is freed in the mike_destroy(mike*) function
-	mike->space = space;
-	double head_mass = mike_head_radius * mike_head_radius * CP_PI * mike_head_density;
-	mike->head = cpBodyNew(head_mass,
-		cpMomentForCircle(head_mass, mike_head_radius, 0, (cpVect){0,0}));
-	mike->head_s = cpCircleShapeNew(mike->head, mike_head_radius, (cpVect){0,0});
+	m->space = NULL;
+	double head_mass = head_radius * head_radius * CP_PI * head_density;
+	m->head = cpBodyNew( head_mass,
+		cpMomentForCircle( head_mass, head_radius, 0, (cpVect){0,0}));
+	m->head_s = cpCircleShapeNew(m->head, head_radius, (cpVect){0,0});
 
-	double upper_leg_mass = mike_upper_leg_length * mike_upper_leg_thickness * mike_upper_leg_density;
-	mike->l = cpBodyNew(upper_leg_mass,
-		cpMomentForBox(upper_leg_mass, mike_upper_leg_thickness, mike_upper_leg_length));
-	mike->r = cpBodyNew(upper_leg_mass,
-		cpMomentForBox(upper_leg_mass, mike_upper_leg_thickness, mike_upper_leg_length));
-	mike->r_s = cpBoxShapeNew(mike->r, mike_upper_leg_thickness, mike_upper_leg_length, 1);
-        mike->l_s = cpBoxShapeNew(mike->l, mike_upper_leg_thickness, mike_upper_leg_length, 1);
+	double upper_leg_mass = upper_leg_length * upper_leg_thickness * upper_leg_density;
+	m->l = cpBodyNew(upper_leg_mass,
+		cpMomentForBox(upper_leg_mass, upper_leg_thickness, upper_leg_length));
+	m->r = cpBodyNew(upper_leg_mass,
+		cpMomentForBox(upper_leg_mass, upper_leg_thickness, upper_leg_length));
+	m->l_s = cpBoxShapeNew(m->l, upper_leg_thickness, upper_leg_length, 0);
+	m->r_s = cpBoxShapeNew(m->r, upper_leg_thickness, upper_leg_length, 0);
 
-	double lower_leg_mass = mike_lower_leg_length * mike_lower_leg_thickness * mike_lower_leg_density;
-	mike->ll = cpBodyNew(lower_leg_mass,
-		cpMomentForBox(lower_leg_mass, mike_lower_leg_thickness, mike_lower_leg_length));
-	mike->rr = cpBodyNew(lower_leg_mass,
-		cpMomentForBox(lower_leg_mass, mike_lower_leg_thickness, mike_lower_leg_length));
-	mike->ll_s = cpBoxShapeNew(mike->ll, mike_lower_leg_thickness, mike_lower_leg_length, 1); // ultimo arg è il "radius" (Adding a small radius will bevel the corners and can significantly reduce problems where the box gets stuck on seams in your geometry)
-	mike->rr_s = cpBoxShapeNew(mike->rr, mike_lower_leg_thickness, mike_lower_leg_length, 1);
-	
-	//cpShapeFilter only_ground = cpShapeFilterNew(1, 0, 0);
-	
-        //mike->l_s->filter =  // (uint8_t)0b00000000, (uint8_t)0b11111111); // cpGroup group, cpBitmask categories, cpBitmask mask. docs: cpShape.h
-	
-	//LEET number cause the filter group aint no bitmask
-        mike->l_s->filter.group = 1337;
-	mike->ll_s->filter.group = 1337;
-	mike->r_s->filter.group = 1337;
-	mike->rr_s->filter.group = 1337;
-	mike->head_s->filter.group = 1337;
+	double lower_leg_mass = lower_leg_length * lower_leg_thickness * lower_leg_density;
+	m->ll = cpBodyNew(lower_leg_mass,
+		cpMomentForBox(lower_leg_mass, lower_leg_thickness, lower_leg_length));
+	m->rr = cpBodyNew(lower_leg_mass,
+		cpMomentForBox(lower_leg_mass, lower_leg_thickness, lower_leg_length));
+	m->ll_s = cpBoxShapeNew(m->ll, lower_leg_thickness, lower_leg_length, 0);
+	m->rr_s = cpBoxShapeNew(m->rr, lower_leg_thickness, lower_leg_length, 0);
 
-	cpShapeSetFriction(mike->head_s, mike_friction);
-	cpShapeSetFriction(mike->l_s, mike_friction);
-	cpShapeSetFriction(mike->ll_s, mike_friction);
-	cpShapeSetFriction(mike->r_s, mike_friction);
-	cpShapeSetFriction(mike->rr_s, mike_friction);
+	cpShapeFilter filter = cpShapeFilterNew(MIKE_COLLISION_GROUP, CP_ALL_CATEGORIES, CP_ALL_CATEGORIES);
 
-	mike->head->p = (cpVect){0,0};
-	mike->l->p = (cpVect){-(mike_head_radius + mike_upper_leg_thickness / 2), -mike_upper_leg_length * 0.5};
-	mike->r->p = (cpVect){(mike_head_radius + mike_upper_leg_thickness / 2), -mike_upper_leg_length * 0.5};
-	mike->ll->p = (cpVect){-(mike_head_radius + mike_upper_leg_thickness / 2), -mike_upper_leg_length - mike_lower_leg_length * 0.5};
-	mike->rr->p = (cpVect){(mike_head_radius + mike_upper_leg_thickness / 2), -mike_upper_leg_length - mike_lower_leg_length * 0.5};
+	cpShapeSetFilter(m->l_s, filter);
+	cpShapeSetFilter(m->ll_s, filter);
+	cpShapeSetFilter(m->r_s, filter);
+	cpShapeSetFilter(m->rr_s, filter);
+	cpShapeSetFilter(m->head_s, filter);
 
-	mike->l_c = cpPivotJointNew(mike->l, mike->head, (cpVect){-(mike_head_radius + mike_upper_leg_thickness / 2), 0});
-	mike->ll_c = cpPivotJointNew(mike->ll, mike->l, (cpVect){-(mike_head_radius + mike_upper_leg_thickness / 2), -mike_upper_leg_length});
-	mike->r_c = cpPivotJointNew(mike->r, mike->head, (cpVect){(mike_head_radius + mike_upper_leg_thickness / 2), 0});
-	mike->rr_c = cpPivotJointNew(mike->rr, mike->r, (cpVect){(mike_head_radius + mike_upper_leg_thickness / 2), -mike_upper_leg_length});
+	cpShapeSetFriction(m->head_s, friction);
+	cpShapeSetFriction(m->l_s, friction);
+	cpShapeSetFriction(m->ll_s, friction);
+	cpShapeSetFriction(m->r_s, friction);
+	cpShapeSetFriction(m->rr_s, friction);
+
+	cpBodySetPosition(m->head, (cpVect){0, 0});
+	cpBodySetPosition(m->l, (cpVect){-(head_radius + upper_leg_thickness / 2), -upper_leg_length * 0.5});
+	cpBodySetPosition(m->r, (cpVect){(head_radius + upper_leg_thickness / 2), -upper_leg_length * 0.5});
+	cpBodySetPosition(m->ll, (cpVect){-(head_radius + upper_leg_thickness / 2), -upper_leg_length - lower_leg_length * 0.5});
+	cpBodySetPosition(m->rr, (cpVect){(head_radius + upper_leg_thickness / 2), -upper_leg_length - lower_leg_length * 0.5});
+
+	m->l_c = cpPivotJointNew(m->l, m->head, (cpVect){-(head_radius + upper_leg_thickness / 2), 0});
+	m->ll_c = cpPivotJointNew(m->ll, m->l, (cpVect){-(head_radius + upper_leg_thickness / 2), -upper_leg_length});
+	m->r_c = cpPivotJointNew(m->r, m->head, (cpVect){(head_radius + upper_leg_thickness / 2), 0});
+	m->rr_c = cpPivotJointNew(m->rr, m->r, (cpVect){(head_radius + upper_leg_thickness / 2), -upper_leg_length});
 
 
-	mike->l_m = cpDampedRotarySpringNew(mike->l, mike->head, 0, mike_upper_joint_stiffness, mike_upper_joint_damping);
-	mike->ll_m = cpDampedRotarySpringNew(mike->ll, mike->l, 0, mike_lower_joint_stiffness, mike_lower_joint_damping);
-	mike->r_m = cpDampedRotarySpringNew(mike->r, mike->head, 0, mike_upper_joint_stiffness, mike_upper_joint_damping);
-	mike->rr_m = cpDampedRotarySpringNew(mike->rr, mike->r, 0, mike_lower_joint_stiffness, mike_lower_joint_damping);
-
-
-	cpSpaceAddConstraint(space, mike->l_c);
-	cpSpaceAddConstraint(space, mike->ll_c);
-	cpSpaceAddConstraint(space, mike->r_c);
-	cpSpaceAddConstraint(space, mike->rr_c);
-
-	cpSpaceAddConstraint(space, mike->l_m);
-	cpSpaceAddConstraint(space, mike->ll_m);
-	cpSpaceAddConstraint(space, mike->r_m);
-	cpSpaceAddConstraint(space, mike->rr_m);
-
-	cpSpaceAddBody(space, mike->l);
-	cpSpaceAddShape(space, mike->l_s);
-	cpSpaceAddBody(space, mike->ll);
-	cpSpaceAddShape(space, mike->ll_s);
-	cpSpaceAddBody(space, mike->r);
-	cpSpaceAddShape(space, mike->r_s);
-	cpSpaceAddBody(space, mike->rr);
-	cpSpaceAddShape(space, mike->rr_s);
-	cpSpaceAddBody(space, mike->head);
-	cpSpaceAddShape(space, mike->head_s);
-
-	mike_reset(mike, x, y);
+	m->l_m = cpDampedRotarySpringNew(m->l, m->head, 0, upper_joint_stiffness, upper_joint_damping);
+	m->ll_m = cpDampedRotarySpringNew(m->ll, m->l, 0, lower_joint_stiffness, lower_joint_damping);
+	m->r_m = cpDampedRotarySpringNew(m->r, m->head, 0, upper_joint_stiffness, upper_joint_damping);
+	m->rr_m = cpDampedRotarySpringNew(m->rr, m->r, 0, lower_joint_stiffness, lower_joint_damping);
 }
 
-int main(){
-        printf("ddd\n");
+void mike_spawn(mike_s *m, cpSpace* space, double x, double y)
+{
+	if(m->space){
+		fprintf(stderr, "mike already spawned in %p, cannot spawn in %p", m->space, space);
+		abort();
+	}
+	m->space = space;
+	cpSpaceAddConstraint(space, m->l_c);
+	cpSpaceAddConstraint(space, m->ll_c);
+	cpSpaceAddConstraint(space, m->r_c);
+	cpSpaceAddConstraint(space, m->rr_c);
+
+	cpSpaceAddConstraint(space, m->l_m);
+	cpSpaceAddConstraint(space, m->ll_m);
+	cpSpaceAddConstraint(space, m->r_m);
+	cpSpaceAddConstraint(space, m->rr_m);
+
+	cpSpaceAddBody(space, m->l);
+	cpSpaceAddShape(space, m->l_s);
+	cpSpaceAddBody(space, m->ll);
+	cpSpaceAddShape(space, m->ll_s);
+	cpSpaceAddBody(space, m->r);
+	cpSpaceAddShape(space, m->r_s);
+	cpSpaceAddBody(space, m->rr);
+	cpSpaceAddShape(space, m->rr_s);
+	cpSpaceAddBody(space, m->head);
+	cpSpaceAddShape(space, m->head_s);
+
+	mike_reset(m, x, y);
+}
+
+void mike_reset(mike_s *m, double x, double y)
+{
+	cpBodySetPosition(m->head, (cpVect){x,y});
+	cpBodySetPosition(m->l, (cpVect){-(head_radius + upper_leg_thickness / 2) + x, -upper_leg_length * 0.5 + y});
+	cpBodySetPosition(m->r, (cpVect){(head_radius + upper_leg_thickness / 2) + x, -upper_leg_length * 0.5 + y});
+	cpBodySetPosition(m->ll, (cpVect){-(head_radius + upper_leg_thickness / 2) + x, -upper_leg_length - lower_leg_length * 0.5 + y});
+	cpBodySetPosition(m->rr, (cpVect){(head_radius + upper_leg_thickness / 2) + x, -upper_leg_length - lower_leg_length * 0.5 + y});
+
+	cpBodySetVelocity(m->head, (cpVect){0, 0});
+	cpBodySetVelocity(m->l, (cpVect){0, 0});
+	cpBodySetVelocity(m->ll,  (cpVect){0, 0});
+	cpBodySetVelocity(m->r, (cpVect){0, 0});
+	cpBodySetVelocity(m->rr,  (cpVect){0, 0});
+
+
+	cpBodySetAngularVelocity(m->head, 0);
+	cpBodySetAngularVelocity(m->l, 0);
+	cpBodySetAngularVelocity(m->ll, 0);
+	cpBodySetAngularVelocity(m->r, 0);
+	cpBodySetAngularVelocity(m->rr, 0);
+
+	cpBodySetAngle(m->head, 0);
+	cpBodySetAngle(m->l, 0);
+	cpBodySetAngle(m->ll, 0);
+	cpBodySetAngle(m->r, 0);
+	cpBodySetAngle(m->rr, 0);
+
+	if (m->space){
+		cpSpaceReindexShapesForBody(m->space, m->l);
+		cpSpaceReindexShapesForBody(m->space, m->ll);
+		cpSpaceReindexShapesForBody(m->space, m->r);
+		cpSpaceReindexShapesForBody(m->space, m->rr);
+		cpSpaceReindexShapesForBody(m->space, m->head);
+	}
+}
+void mike_despawn(mike_s *m)
+{
+	if(m->space){
+		cpSpaceRemoveBody(m->space, m->head);
+		cpSpaceRemoveBody(m->space, m->l);
+		cpSpaceRemoveBody(m->space, m->ll);
+		cpSpaceRemoveBody(m->space, m->r);
+		cpSpaceRemoveBody(m->space, m->rr);
+
+		cpSpaceRemoveShape(m->space, m->head_s);
+		cpSpaceRemoveShape(m->space, m->l_s);
+		cpSpaceRemoveShape(m->space, m->ll_s);
+		cpSpaceRemoveShape(m->space, m->r_s);
+		cpSpaceRemoveShape(m->space, m->rr_s);
+
+		cpSpaceRemoveConstraint(m->space, m->l_c);
+		cpSpaceRemoveConstraint(m->space, m->ll_c);
+		cpSpaceRemoveConstraint(m->space, m->r_c);
+		cpSpaceRemoveConstraint(m->space, m->rr_c);
+
+		cpSpaceRemoveConstraint(m->space, m->l_m);
+		cpSpaceRemoveConstraint(m->space, m->ll_m);
+		cpSpaceRemoveConstraint(m->space, m->r_m);
+		cpSpaceRemoveConstraint(m->space, m->rr_m);
+	}
+}
+
+void mike_free(mike_s *m)
+{
+	mike_despawn(m);
+	cpBodyFree(m->head);
+	cpBodyFree(m->l);
+	cpBodyFree(m->ll);
+	cpBodyFree(m->r);
+	cpBodyFree(m->rr);
+
+	cpShapeFree(m->head_s);
+	cpShapeFree(m->l_s);
+	cpShapeFree(m->ll_s);
+	cpShapeFree(m->r_s);
+	cpShapeFree(m->rr_s);
+
+	cpConstraintFree(m->l_c);
+	cpConstraintFree(m->ll_c);
+	cpConstraintFree(m->r_c);
+	cpConstraintFree(m->rr_c);
+
+	cpConstraintFree(m->l_m);
+	cpConstraintFree(m->ll_m);
+	cpConstraintFree(m->r_m);
+	cpConstraintFree(m->rr_m);
+}
+
+void mike_brain_inputs(mike_s *m, double *output)
+{
+	output[0] =	1;
+	output[1] =	cpBodyGetPosition(m->head).y / 70. - 0.3; //why 0.3?
+	output[2] =	(cpBodyGetAngle(m->head) - cpBodyGetAngle(m->l)) / (2 * CP_PI);
+	output[3] =	(cpBodyGetAngle(m->l) - cpBodyGetAngle(m->ll)) / (2 * CP_PI);
+	output[4] =	(cpBodyGetAngle(m->head) - cpBodyGetAngle(m->r)) / (2 * CP_PI);
+	output[5] =	(cpBodyGetAngle(m->r) - cpBodyGetAngle(m->rr)) / (2 * CP_PI);
+	output[6] =	cpBodyGetAngle(m->head) / (2 * CP_PI);
+	output[7] =	(float)(cpShapeGetBB(m->ll_s).b < 6.);
+	output[8] =	(float)(cpShapeGetBB(m->rr_s).b < 6.);
+	output[9] =	cpBodyGetVelocity(m->head).x/10;
+	output[10] =	cpBodyGetVelocity(m->head).y/10;
+}
+
+void mike_muscle_input(mike_s *m, double *arr)
+{
+	cpDampedRotarySpringSetRestAngle(m->l_m, (arr[0] + 1) * CP_PI);
+	cpDampedRotarySpringSetRestAngle(m->ll_m, (arr[1] + 1) * CP_PI);
+	cpDampedRotarySpringSetRestAngle(m->r_m, (arr[2] + 1) * CP_PI);
+	cpDampedRotarySpringSetRestAngle(m->rr_m, (arr[3] + 1) * CP_PI);
 }
--- mike.h
+++ mike.h
@@ -1,18 +1,53 @@
 #include <chipmunk/chipmunk.h>
 
-struct mike{
-        cpBody *head, *l, *r, *ll, *rr;
+typedef struct mike{
+	cpBody *head, *l, *r, *ll, *rr;
 	cpShape *head_s, *l_s, *r_s, *ll_s, *rr_s;
 	cpConstraint	*l_c, *ll_c, *r_c, *rr_c,
 			*l_m, *ll_m, *r_m, *rr_m;
 	cpSpace *space;
-};
-typedef struct mike mike_s;
+} mike_s;
 
+void mike_free(mike_s *m);
+//initializes mike
+void mike_init(mike_s *m);
+//adds mike to a cpSpace. Cannot add the same mike twice.
+void mike_spawn(mike_s *m, cpSpace* space, double x, double y);
+//removes mike to the space he is currently in
+void mike_despawn(mike_s *m);
+//puts into output what mike senses. The sizeo of outputs is hard coded. (BAD)
+void mike_brain_inputs(mike_s *m, double *output);
+//how muscles should move
+void mike_muscle_input(mike_s *m, double *input);
+//places mike in a still standing position at coordinates x,y
+void mike_reset(mike_s *m, double x, double y);
 
-void mike_free(mike_s *mike);
-void mike_init(mike_s *mike, cpSpace* space, float x, float y);
-void mike_brain_input(mike_s *mike, double target, float *brain_inputs);
-void mike_muscle_input(mike_s *mike, const float *arr);
-void mike_despawn(mike_s *mike);
-void mike_reset(mike_s *mike, float x, float y);
+/*
+void draw_mike(sf::RenderWindow &window, Mike &m, double target)
+{
+	sf::CircleShape head {(float)m.head_radius};
+	head.setOrigin(m.head_radius, m.head_radius);
+	head.setPosition(cpBodyGetPosition(m.head).x, cpBodyGetPosition(m.head).y);
+	window.draw(head);
+	// debug target loc
+	head = sf::CircleShape {3};
+	head.setPosition(target, 100);
+	window.draw(head);
+	//
+	sf::RectangleShape upper_leg {sf::Vector2f{(float)m.upper_leg_thickness, (float)m.upper_leg_length}};
+	sf::RectangleShape lower_leg {sf::Vector2f{(float)m.lower_leg_thickness, (float)m.lower_leg_length}};
+	upper_leg.setOrigin(m.upper_leg_thickness/2., m.upper_leg_length/2.);
+	lower_leg.setOrigin(m.lower_leg_thickness/2., m.lower_leg_length/2.);
+	upper_leg.setPosition(cpBodyGetPosition(m.l).x, cpBodyGetPosition(m.l).y);
+	lower_leg.setPosition(cpBodyGetPosition(m.ll).x, cpBodyGetPosition(m.ll).y);
+	upper_leg.setRotation(cpBodyGetAngle(m.l) / (2 * M_PI) * 360);
+	lower_leg.setRotation(cpBodyGetAngle(m.ll) / (2 * M_PI) * 360);
+	window.draw(upper_leg);
+	window.draw(lower_leg);
+	upper_leg.setPosition(cpBodyGetPosition(m.r).x, cpBodyGetPosition(m.r).y);
+	lower_leg.setPosition(cpBodyGetPosition(m.rr).x, cpBodyGetPosition(m.rr).y);
+	upper_leg.setRotation(cpBodyGetAngle(m.r) / (2 * M_PI) * 360);
+	lower_leg.setRotation(cpBodyGetAngle(m.rr) / (2 * M_PI) * 360);
+	window.draw(upper_leg);
+	window.draw(lower_leg);
+}*/
